57a3ee1436b8bd6dcc3eea25ddd8f9f4c0aea05c
Hussain <hussainmurtaza.cse_b2019@crescent.education>
2022-02-27 12:37:21 +0530

    db.js modified

--- db.js
+++ db.js
@@ -1,19 +1,26 @@
 const mongoose = require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 // const mongoURi = 'mongodb://localhost:27017/CNote?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
 const username = encodeURIComponent("Hussain");
 const password = encodeURIComponent("Hussain@7860");
 const mongoURi = `mongodb+srv://${username}:${password}@cluster0.qv5zj.mongodb.net/test`
 const connectToMongo = () => {
     mongoose.connect(mongoURi, {
-        userNewUrlPaser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
     }).then(()=>{
         console.log('connected to mongo successfully');
     }) .catch((e) => {
-        console.log('not connected');
+        console.log(e,'not connected');
       });
 
 }
+
+// const uri = `mongodb+srv://${username}:${password}@cluster0.qv5zj.mongodb.net/Cluster0?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 module.exports = connectToMongo
