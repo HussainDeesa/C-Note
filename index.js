daa46d0fe64461bd69f6b832470c1c66e3d21ed8
Hussain <hussainmurtaza.cse_b2019@crescent.education>
2022-02-27 12:03:21 +0530

    process.env  modified

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-if (process.env, NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
